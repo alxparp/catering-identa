e53dcfb3b6033aedb52c070ede0987cb0bad715f
Alex <alex.parpalak@gmail.com>
2023-08-13 10:42:38 +0300

    init

---
+++ src/main/java/com/identa/catering/CateringIdentaApplication.java
@@ -0,0 +1,13 @@
+package com.identa.catering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CateringIdentaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CateringIdentaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/identa/catering/CateringIdentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.identa.catering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CateringIdentaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
